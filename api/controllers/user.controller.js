3227d0f11e91673c4f501f37ec4ca696da4b9656
Saksham <vatssakshu@gmail.com>
2024-03-10 03:48:32 +0530

    complete show user listings functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -65,12 +65,14 @@ export const getUser = async (req, res, next) => {
 };
 
 export const getUserListings = async (req, res, next) => {
-  if (req.user.id !== req.params.id)
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
     return next(errorHandler(401, "You can only view your own listings!"));
-  try {
-    const listings = await Listing.find({ userRef: req.params.id });
-    res.status(200).json(listings);
-  } catch (error) {
-    next(error);
   }
 };
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -21,6 +21,7 @@ import {
   signOutUserSuccess,
 } from "../redux/user/userSlice";
 import { Link } from "react-router-dom";
+import { set } from "mongoose";
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -30,6 +31,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListing, setUserListing] = useState([{}]);
 
   const dispatch = useDispatch();
 
@@ -121,6 +124,20 @@ export default function Profile() {
       dispatch(signOutUserFailure(error.message));
     }
   };
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListing(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -201,6 +218,44 @@ export default function Profile() {
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User Updated Successfully" : ""}
       </p>
+      <button onClick={handleShowListings} className="text-green-700">
+        Show Listings
+      </button>
+      <p className="text-red-700 mt-5">
+        {showListingsError ? "Error fetching listings" : ""}
+      </p>
+      {userListing && userListing.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-3xl font-semibold">
+            Your Listings
+          </h1>
+          {userListing.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls}
+                  alt="listing-cover"
+                  className="h-16 w-16 object-contain"
+                ></img>
+              </Link>
+
+              <Link
+                className="text-slate-700 font-semibold flex-1 hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className="flex flex-col items-center">
+                <button className="text-red-700 uppercase">Delete</button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
